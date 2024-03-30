2e1f507ee58bc9f80d334ad7d48fb66e3cf4375a
Diyorbek <muhammadjonovd793@gmail.com>
2024-03-30 16:08:25 +0500

    Added Studio

--- src/OneFit.Domain/Commons/Auditable.cs
+++ src/OneFit.Domain/Commons/Auditable.cs
@@ -2,5 +2,9 @@ namespace OneFit.Domain.Commons;
 
 public class Auditable
 {
-    
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+    public bool IsDeleted { get; set; } = false;
 }
