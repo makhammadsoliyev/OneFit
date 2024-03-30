73dd9f525f7f7098a576bbc3b324adecee5d732e
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-03-30 02:21:58 -0700

    Created User Entity and dtos

---
+++ src/OneFit.Service/DTOs/UserUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OneFit.Service.DTOs;
+
+public class UserUpdateModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+}
