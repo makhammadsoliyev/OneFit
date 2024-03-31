f7766cd1e1f2f9cf75ef2bb5cc217a4716816456
Diyorbek <muhammadjonovd793@gmail.com>
2024-03-31 21:49:22 +0500

    Create Studio Controller

---
+++ src/OneFit.WebApi/Controllers/StudiosController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneFit.Service.DTOs.Studios;
+using OneFit.Service.Services.Studios;
+using OneFit.WebApi.Models;
+
+namespace OneFit.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudiosController : ControllerBase
+    {
+        private readonly IStudioService studioService;
+
+        public StudiosController(IStudioService studioService)
+        {
+            this.studioService = studioService;
+        }
+
+        // GET: api/<StudiosController>
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(new Response()
+            {
+                StatusCode = 200,
+                Message = "OK",
+                Data = await studioService.GetAllAsync()
+            });
+        }
+
+        // GET api/<StudiosController>/
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(long id)
+        {
+            return Ok(new Response()
+            {
+                StatusCode = 200,
+                Message = "OK",
+                Data = await studioService.GetByIdAsync(id)
+            });
+        }
+
+        // POST api/<StudiosController>
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] StudioCreateModel studio)
+        {
+            return Ok(new Response()
+            {
+                StatusCode = 200,
+                Message = "OK",
+                Data = await studioService.CreateAsync(studio)
+            });
+        }
+
+        // PUT api/<StudiosController>/
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutASync(long id, [FromBody] StudioUpdateModel studio)
+        {
+            return Ok(new Response()
+            {
+                StatusCode = 200,
+                Message = "OK",
+                Data = await studioService.UpdateAsync(id, studio)
+            });
+        }
+
+        // DELETE api/<StudiosController>/
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            return Ok(new Response()
+            {
+                StatusCode = 200,
+                Message = "OK",
+                Data = await studioService.DeleteAsync(id)
+            });
+        }
+
+    }
+}
