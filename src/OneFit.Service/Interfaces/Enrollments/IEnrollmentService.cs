7a2bfba8e47f1dd40020c73675bfc64c308fea73
Jismatov895 <Jismatov895@gmail.com>
2024-03-31 13:25:12 +0500

    Added Enrollment Interface

---
+++ src/OneFit.Service/Interfaces/Enrollments/IEnrollmentService.cs
@@ -0,0 +1,12 @@
+﻿using OneFit.Service.DTOs.Enrollments;
+
+namespace OneFit.Service.Interfaces.Enrollments
+{
+    public interface IEnrollmentService
+    {
+        Task<EnrollmentViewModel> CreateAsync(EnrollmentCreateModel issue);
+        Task<bool> DeleteAsync(long id);
+        Task<EnrollmentViewModel> GetByIdAsync(long id);
+        Task<IEnumerable<EnrollmentViewModel>> GetAllAsync();
+    }
+}
